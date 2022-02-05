7f7f8a49a84563a241dffc7716466216bc9d65e0
cr1m3 <jeffryjulianto7@gmail.com>
2022-02-05 20:51:05 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Jeffry Julianto',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Jeffry Julianto is a electric PSD engineer who specializes in power system, control, and maintenance.',
+    siteUrl: 'https://jejuliantos.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@jejuliantos',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
