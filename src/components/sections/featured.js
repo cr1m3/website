e3a22b67699c746fe22812f30f8a37c01643598d
cr1m3 <jeffryjulianto7@gmail.com>
2023-01-06 19:51:14 +0700

    Update featured.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -1,107 +1,295 @@
 import React, { useEffect, useRef } from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import styled from 'styled-components';
-import { srConfig } from '@config';
 import sr from '@utils/sr';
+import { srConfig } from '@config';
+import { Icon } from '@components/icons';
 import { usePrefersReducedMotion } from '@hooks';
 
-const StyledAboutSection = styled.section`
-  max-width: 900px;
-  .inner {
-    display: grid;
-    grid-template-columns: 3fr 2fr;
-    grid-gap: 50px;
-    @media (max-width: 768px) {
-      display: block;
-    }
+const StyledProjectsGrid = styled.ul`
+  ${({ theme }) => theme.mixins.resetList};
+  a {
+    position: relative;
+    z-index: 1;
   }
 `;
-const StyledText = styled.div`
-  ul.skills-list {
-    display: grid;
-    grid-template-columns: repeat(2, minmax(140px, 200px));
-    grid-gap: 0 10px;
+
+const StyledProject = styled.li`
+  position: relative;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(12, 1fr);
+  align-items: center;
+  @media (max-width: 768px) {
+    ${({ theme }) => theme.mixins.boxShadow};
+  }
+  &:not(:last-of-type) {
+    margin-bottom: 100px;
+    @media (max-width: 768px) {
+      margin-bottom: 70px;
+    }
+    @media (max-width: 480px) {
+      margin-bottom: 30px;
+    }
+  }
+  &:nth-of-type(odd) {
+    .project-content {
+      grid-column: 7 / -1;
+      text-align: right;
+      @media (max-width: 1080px) {
+        grid-column: 5 / -1;
+      }
+      @media (max-width: 768px) {
+        grid-column: 1 / -1;
+        padding: 40px 40px 30px;
+        text-align: left;
+      }
+      @media (max-width: 480px) {
+        padding: 25px 25px 20px;
+      }
+    }
+    .project-tech-list {
+      justify-content: flex-end;
+      @media (max-width: 768px) {
+        justify-content: flex-start;
+      }
+      li {
+        margin: 0 0 5px 20px;
+        @media (max-width: 768px) {
+          margin: 0 10px 5px 0;
+        }
+      }
+    }
+    .project-links {
+      justify-content: flex-end;
+      margin-left: 0;
+      margin-right: -10px;
+      @media (max-width: 768px) {
+        justify-content: flex-start;
+        margin-left: -10px;
+        margin-right: 0;
+      }
+    }
+    .project-image {
+      grid-column: 1 / 8;
+      @media (max-width: 768px) {
+        grid-column: 1 / -1;
+      }
+    }
+  }
+  .project-content {
+    position: relative;
+    grid-column: 1 / 7;
+    grid-row: 1 / -1;
+    @media (max-width: 1080px) {
+      grid-column: 1 / 9;
+    }
+    @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      height: 100%;
+      grid-column: 1 / -1;
+      padding: 40px 40px 30px;
+      z-index: 5;
+    }
+    @media (max-width: 480px) {
+      padding: 30px 25px 20px;
+    }
+  }
+  .project-overline {
+    margin: 10px 0;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: var(--fz-xs);
+    font-weight: 400;
+  }
+  .project-title {
+    color: var(--lightest-slate);
+    font-size: clamp(24px, 5vw, 28px);
+    @media (min-width: 768px) {
+      margin: 0 0 20px;
+    }
+    @media (max-width: 768px) {
+      color: var(--white);
+      a {
+        position: static;
+        &:before {
+          content: '';
+          display: block;
+          position: absolute;
+          z-index: 0;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+        }
+      }
+    }
+  }
+  .project-description {
+    ${({ theme }) => theme.mixins.boxShadow};
+    position: relative;
+    z-index: 2;
+    padding: 25px;
+    border-radius: var(--border-radius);
+    background-color: var(--light-navy);
+    color: var(--light-slate);
+    font-size: var(--fz-lg);
+    @media (max-width: 768px) {
+      padding: 20px 0;
+      background-color: transparent;
+      box-shadow: none;
+      &:hover {
+        box-shadow: none;
+      }
+    }
+    a {
+      ${({ theme }) => theme.mixins.inlineLink};
+    }
+    strong {
+      color: var(--white);
+      font-weight: normal;
+    }
+  }
+  .project-tech-list {
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+    z-index: 2;
+    margin: 25px 0 10px;
     padding: 0;
-    margin: 20px 0 0 0;
-    overflow: hidden;
     list-style: none;
     li {
-      position: relative;
-      margin-bottom: 10px;
-      padding-left: 20px;
+      margin: 0 20px 5px 0;
+      color: var(--light-slate);
       font-family: var(--font-mono);
       font-size: var(--fz-xs);
-      &:before {
-        content: '▹';
-        position: absolute;
-        left: 0;
-        color: var(--green);
-        font-size: var(--fz-sm);
-        line-height: 12px;
+      white-space: nowrap;
+    }
+    @media (max-width: 768px) {
+      margin: 10px 0;
+      li {
+        margin: 0 10px 5px 0;
+        color: var(--lightest-slate);
       }
     }
   }
-`;
-const StyledPic = styled.div`
-  position: relative;
-  max-width: 300px;
-  @media (max-width: 768px) {
-    margin: 50px auto 0;
-    width: 70%;
-  }
-  .wrapper {
-    ${({ theme }) => theme.mixins.boxShadow};
-    display: block;
+  .project-links {
+    display: flex;
+    align-items: center;
     position: relative;
-    width: 100%;
-    border-radius: var(--border-radius);
-    background-color: var(--green);
-    &:hover,
-    &:focus {
-      outline: 0;
-      &:after {
-        top: 15px;
-        left: 15px;
+    margin-top: 10px;
+    margin-left: -10px;
+    color: var(--lightest-slate);
+    a {
+      ${({ theme }) => theme.mixins.flexCenter};
+      padding: 10px;
+      &.external {
+        svg {
+          width: 22px;
+          height: 22px;
+          margin-top: -4px;
+        }
       }
-      .img {
-        filter: none;
-        mix-blend-mode: normal;
+      svg {
+        width: 20px;
+        height: 20px;
+      }
+    }
+    .cta {
+      ${({ theme }) => theme.mixins.smallButton};
+      margin: 10px;
+    }
+  }
+  .project-image {
+    ${({ theme }) => theme.mixins.boxShadow};
+    grid-column: 6 / -1;
+    grid-row: 1 / -1;
+    position: relative;
+    z-index: 1;
+    @media (max-width: 768px) {
+      grid-column: 1 / -1;
+      height: 100%;
+      opacity: 0.25;
+    }
+    a {
+      width: 100%;
+      height: 100%;
+      background-color: var(--green);
+      border-radius: var(--border-radius);
+      vertical-align: middle;
+      &:hover,
+      &:focus {
+        background: transparent;
+        outline: 0;
+        &:before,
+        .img {
+          background: transparent;
+          filter: none;
+        }
+      }
+      &:before {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 3;
+        transition: var(--transition);
+        background-color: var(--navy);
+        mix-blend-mode: screen;
       }
     }
     .img {
-      position: relative;
       border-radius: var(--border-radius);
       mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1);
-      transition: var(--transition);
-    }
-    &:before,
-    &:after {
-      content: '';
-      display: block;
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      border-radius: var(--border-radius);
-      transition: var(--transition);
-    }
-    &:before {
-      top: 0;
-      left: 0;
-      background-color: var(--navy);
-      mix-blend-mode: screen;
-    }
-    &:after {
-      border: 2px solid var(--green);
-      top: 20px;
-      left: 20px;
-      z-index: -1;
+      filter: grayscale(100%) contrast(1) brightness(90%);
+      @media (max-width: 768px) {
+        object-fit: cover;
+        width: auto;
+        height: 100%;
+        filter: grayscale(100%) contrast(1) brightness(50%);
+      }
     }
   }
 `;
 
-const About = () => {
-  const revealContainer = useRef(null);
+const Featured = () => {
+  const data = useStaticQuery(graphql`
+    {
+      featured: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/content/featured/" } }
+        sort: { fields: [frontmatter___date], order: ASC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              cover {
+                childImageSharp {
+                  gatsbyImageData(width: 700, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+                }
+              }
+              tech
+              github
+              external
+              cta
+            }
+            html
+          }
+        }
+      }
+    }
+  `);
+
+  const featuredProjects = data.featured.edges.filter(({ node }) => node);
+  const revealTitle = useRef(null);
+  const revealProjects = useRef([]);
   const prefersReducedMotion = usePrefersReducedMotion();
 
   useEffect(() => {
@@ -109,68 +297,77 @@ const About = () => {
       return;
     }
 
-    sr.reveal(revealContainer.current, srConfig());
+    sr.reveal(revealTitle.current, srConfig());
+    revealProjects.current.forEach((ref, i) => sr.reveal(ref, srConfig(i * 100)));
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
-
   return (
-    <StyledAboutSection id="about" ref={revealContainer}>
-      <h2 className="numbered-heading">About Me</h2>
+    <section id="projects">
+      <h2 className="numbered-heading" ref={revealTitle}>
+        Some Things I’ve Built
+      </h2>
 
-      <div className="inner">
-        <StyledText>
-          <div>
-            <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
+      <StyledProjectsGrid>
+        {featuredProjects &&
+          featuredProjects.map(({ node }, i) => {
+            const { frontmatter, html } = node;
+            const { external, title, tech, github, cover, cta } = frontmatter;
+            const image = getImage(cover);
 
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
+            return (
+              <StyledProject key={i} ref={el => (revealProjects.current[i] = el)}>
+                <div className="project-content">
+                  <div>
+                    <p className="project-overline">Featured Project</p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
+                    <h3 className="project-title">
+                      <a href={external}>{title}</a>
+                    </h3>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
-          </div>
+                    <div
+                      className="project-description"
+                      dangerouslySetInnerHTML={{ __html: html }}
+                    />
 
-          <ul className="skills-list">
-            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-          </ul>
-        </StyledText>
+                    {tech.length && (
+                      <ul className="project-tech-list">
+                        {tech.map((tech, i) => (
+                          <li key={i}>{tech}</li>
+                        ))}
+                      </ul>
+                    )}
 
-        <StyledPic>
-          <div className="wrapper">
-            <StaticImage
-              className="img"
-              src="../../images/me.jpg"
-              width={500}
-              quality={95}
-              formats={['AUTO', 'WEBP', 'AVIF']}
-              alt="Headshot"
-            />
-          </div>
-        </StyledPic>
-      </div>
-    </StyledAboutSection>
+                    <div className="project-links">
+                      {cta && (
+                        <a href={cta} aria-label="Course Link" className="cta">
+                          Learn More
+                        </a>
+                      )}
+                      {github && (
+                        <a href={github} aria-label="GitHub Link">
+                          <Icon name="GitHub" />
+                        </a>
+                      )}
+                      {external && !cta && (
+                        <a href={external} aria-label="External Link" className="external">
+                          <Icon name="External" />
+                        </a>
+                      )}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="project-image">
+                  <a href={external ? external : github ? github : '#'}>
+                    <GatsbyImage image={image} alt={title} className="img" />
+                  </a>
+                </div>
+              </StyledProject>
+            );
+          })}
+      </StyledProjectsGrid>
+    </section>
   );
 };
 
-export default About;
+export default Featured;
