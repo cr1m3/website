2788240de002ca82b6d2de4a3a7f4d95ae499b90
cr1m3 <jeffryjulianto7@gmail.com>
2023-01-06 22:00:07 +0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -7,12 +7,10 @@ import { usePrefersReducedMotion } from '@hooks';
 
 const StyledAboutSection = styled.section`
   max-width: 900px;
-  
-    .inner {
+  .inner {
     display: grid;
     grid-template-columns: 3fr 2fr;
     grid-gap: 50px;
-    
     @media (max-width: 768px) {
       display: block;
     }
@@ -27,15 +25,13 @@ const StyledText = styled.div`
     margin: 20px 0 0 0;
     overflow: hidden;
     list-style: none;
-    
     li {
       position: relative;
       margin-bottom: 10px;
       padding-left: 20px;
       font-family: var(--font-mono);
       font-size: var(--fz-xs);
-
-&:before {
+      &:before {
         content: '▹';
         position: absolute;
         left: 0;
@@ -49,44 +45,37 @@ const StyledText = styled.div`
 const StyledPic = styled.div`
   position: relative;
   max-width: 300px;
-  
   @media (max-width: 768px) {
     margin: 50px auto 0;
     width: 70%;
   }
-  
-    .wrapper {
+  .wrapper {
     ${({ theme }) => theme.mixins.boxShadow};
     display: block;
     position: relative;
     width: 100%;
     border-radius: var(--border-radius);
     background-color: var(--green);
-    
-        &:hover,
+    &:hover,
     &:focus {
       outline: 0;
-      
-            &:after {
+      &:after {
         top: 15px;
         left: 15px;
       }
-      
-            .img {
+      .img {
         filter: none;
         mix-blend-mode: normal;
       }
     }
-    
-        .img {
+    .img {
       position: relative;
       border-radius: var(--border-radius);
       mix-blend-mode: multiply;
       filter: grayscale(100%) contrast(1);
       transition: var(--transition);
     }
-    
-        &:before,
+    &:before,
     &:after {
       content: '';
       display: block;
@@ -96,15 +85,13 @@ const StyledPic = styled.div`
       border-radius: var(--border-radius);
       transition: var(--transition);
     }
-
     &:before {
       top: 0;
       left: 0;
       background-color: var(--navy);
       mix-blend-mode: screen;
     }
-    
-        &:after {
+    &:after {
       border: 2px solid var(--green);
       top: 20px;
       left: 20px;
@@ -116,27 +103,53 @@ const StyledPic = styled.div`
 const About = () => {
   const revealContainer = useRef(null);
   const prefersReducedMotion = usePrefersReducedMotion();
-  
-    useEffect(() => {
+
+  useEffect(() => {
     if (prefersReducedMotion) {
       return;
     }
 
-          sr.reveal(revealContainer.current, srConfig());
+    sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-    const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
-  
-    return (
+  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+
+  return (
     <StyledAboutSection id="about" ref={revealContainer}>
       <h2 className="numbered-heading">About Me</h2>
 
       <div className="inner">
         <StyledText>
           <div>
+            <p>
+              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2012 when I decided to try editing custom
+              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
+              about HTML &amp; CSS!
+            </p>
 
-            <p>Here are a few presentation I’ve been working with recently:</p>
-            </div>
+            <p>
+              Fast-forward to today, and I’ve had the privilege of working at{' '}
+              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
+              <a href="https://starry.com/">a start-up</a>,{' '}
+              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
+              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+              main focus these days is building accessible, inclusive products and digital
+              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+              clients.
+            </p>
+
+            <p>
+              I also recently{' '}
+              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
+                launched a course
+              </a>{' '}
+              that covers everything you need to build a web app with the Spotify API using Node
+              &amp; React.
+            </p>
+
+            <p>Here are a few technologies I’ve been working with recently:</p>
+          </div>
 
           <ul className="skills-list">
             {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
