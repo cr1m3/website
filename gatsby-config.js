12103cbd91df7c8a8f2737dfb089a75e2253532a
cr1m3 <jeffryjulianto7@gmail.com>
2023-01-08 21:28:33 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,11 @@ module.exports = {
     twitterUsername: '@jejuliantos',
   },
   plugins: [
-    'gatsby-plugin-netlify',
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     {
