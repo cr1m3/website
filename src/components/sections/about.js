a3958bc608c5f89a0dafdecdc0bd29f8bf680275
cr1m3 <jeffryjulianto7@gmail.com>
2023-06-23 21:52:21 +0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,18 +136,17 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Jeffry and I am an Electrical Engineer. My Interest in Electrical started
+              back when I was child thinking how my Toys can move by itself, now I know that is because of Battery :D
+              Then in 2015 I decided to learn more about Electrical in Bachelor Degree.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+              <a href="https://www.aprilasia.com/">APRIL Group</a>,{' '}
+              <a href="https://www.chevron.com/">Chevron Pacific Indonesia</a>,{' '}
+              <a href="https://www.pertamina.com/">Pertamina RU II</a>, and{' '}
+              <a href="https://www.unri.ac.id/">T3 LAB</a>. My
               main focus these days is building accessible, inclusive products and digital
               experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
               clients.
