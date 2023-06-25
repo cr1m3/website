37a67b78ceb2bf2f7dcebb34c178d4294dabec9c
cr1m3 <jeffryjulianto7@gmail.com>
2023-06-25 18:13:56 +0700

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -19,6 +19,9 @@ const IconLogo = () => (
         />
       </g>
     </g>
+    <text x="28" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas, serif">
+      J
+    </text>
   </svg>
 );
 
